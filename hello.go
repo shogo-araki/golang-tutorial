23e82c3423a0b362812b0b0485b0c213e1ac62b6
shogo-araki <shogo.araki.shogo.araki@gmail.com>
2025-10-05 13:48:23 +0900

    golang-tutorial

---
+++ hello.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Print("Hello world!")
+	fmt.Println("The time is", time.Now())
+}
---
+++ type-inference.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	v := 42 // change me
+	fmt.Printf("v is of type %T\n", v)
+
+	a := "aaa"
+	fmt.Printf("v is of type %T\n", a)
+
+	b := 3.14
+	fmt.Printf("v is of type %T\n", b)
+
+	c := 0.867 + 0.5i 
+	fmt.Printf("v is of type %T\n", c)
+}
